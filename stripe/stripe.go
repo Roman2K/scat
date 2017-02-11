b3fc4cf4524e4a06445935295e578df31bae18e9
Roman Le Négrate <roman.lenegrate@gmail.com>
2017-02-11 11:03:06 +0100

    stripe: overhaul

--- stripe/stripe.go
+++ stripe/stripe.go
@@ -34,98 +34,113 @@ func (s S) Stripe(dests Locs, seq Seq, distinct, min int) (S, error) {
 	for it, got := range s {
 		items = append(items, it)
 		for loc := range got {
+			if _, ok := dests[loc]; !ok {
+				continue
+			}
 			prios[loc]++
 		}
 	}
 	sortItems(items)
 	res := make(S, len(items))
-	for _, it := range items {
-		got, ok := s[it]
-		if !ok {
-			panic("unknown item")
-		}
-		old := make(Locs, len(got))
-		for k, v := range got {
-			old[k] = v
-		}
-		seen := make(Locs, len(dests))
-		next := func() (loc, error) {
-			for loc := range old {
-				delete(old, loc)
-				return loc, nil
+	for pass := 0; pass < 2; pass++ {
+		for _, it := range items {
+			got, ok := s[it]
+			if !ok {
+				panic("unknown item")
 			}
-			new := seq.Next()
-			if _, ok := seen[new]; ok {
-				delete(prios, new)
-				if len(prios) > 0 {
-					return nil, nil
+			old := make(Locs, len(got))
+			for k, v := range got {
+				old[k] = v
+			}
+			seen := make(Locs, len(dests))
+			next := func() (loc, error) {
+				for loc := range old {
+					delete(old, loc)
+					return loc, nil
 				}
-				return nil, ErrShort
-			}
-			seen.Add(new)
-			return new, nil
-		}
-		newLocs := make(Locs, min)
-		res[it] = newLocs
-		for len(newLocs) < min {
-			new, err := next()
-			if err != nil {
-				return nil, err
-			}
-			if new == nil {
-				continue
-			}
-			if _, ok := got[new]; !ok {
-				if prio, ok := prios[new]; ok {
-					if prio > 0 {
-						prios[new]--
-						delete(seen, new)
-						continue
-					}
+				new := seq.Next()
+				if _, ok := seen[new]; ok {
 					delete(prios, new)
+					if len(prios) > 0 {
+						return nil, nil
+					}
+					return nil, ErrShort
 				}
+				seen.Add(new)
+				return new, nil
 			}
-			if _, ok := newLocs[new]; ok {
-				continue
+			newLocs, ok := res[it]
+			if !ok {
+				newLocs = make(Locs, min)
+				res[it] = newLocs
 			}
-			if _, ok := dests[new]; !ok {
-				continue
-			}
-			newLocs.Add(new)
-			if len(newLocs) <= distinct && !res.exclusive(it) {
-				delete(newLocs, new)
-				continue
+			for len(newLocs) < min {
+				new, err := next()
+				if err != nil {
+					return nil, err
+				}
+				if new == nil {
+					continue
+				}
+				if _, ok := got[new]; !ok {
+					if prio, ok := prios[new]; ok {
+						if prio > 0 {
+							prios[new]--
+							delete(seen, new)
+							continue
+						}
+						delete(prios, new)
+					}
+				}
+				if _, ok := newLocs[new]; ok {
+					continue
+				}
+				if _, ok := dests[new]; !ok {
+					continue
+				}
+				minDistinct := distinct
+				if max := len(res); minDistinct > max {
+					minDistinct = max
+				}
+				newLocs.Add(new)
+				if res.distincts() < minDistinct {
+					delete(newLocs, new)
+					if pass == 0 {
+						break
+					}
+				}
 			}
 		}
 	}
 	for it, got := range s {
+		new := res[it]
 		for old := range got {
-			delete(res[it], old)
+			delete(new, old)
 		}
 	}
 	return res, nil
 }
 
-func (s S) exclusive(it item) bool {
-	locs, ok := s[it]
-	if !ok {
-		return true
+func (s S) distincts() (count int) {
+	counts := map[loc]uint{}
+	for _, locs := range s {
+		for loc := range locs {
+			counts[loc]++
+		}
 	}
-	for it2, otherLocs := range s {
-		if it2 == it {
-			continue
-		}
-		a, b := locs, otherLocs
-		if len(b) < len(a) {
-			a, b = b, a
-		}
-		for loc := range a {
-			if _, ok := b[loc]; ok {
-				return false
+	for _, locs := range s {
+		excl := true
+		for loc := range locs {
+			if counts[loc] > 1 {
+				excl = false
+				break
 			}
 		}
+		if excl {
+			count++
+		}
 	}
-	return true
+	return
 }
 
 type Striper interface {
--- stripe/stripe_test.go
+++ stripe/stripe_test.go
@@ -1,7 +1,13 @@
 package stripe
 
 import (
+	"bufio"
+	"fmt"
+	"regexp"
+	"runtime"
 	"sort"
+	"strconv"
+	"strings"
 	"testing"
 
 	assert "github.com/stretchr/testify/require"
@@ -21,150 +27,225 @@ func TestStripe(t *testing.T) {
 		})
 	}
 
-	seq := &RR{Items: []interface{}{"a", "b", "c"}}
-	dests := NewLocs("a", "b", "c")
+	test(t, `
+		// nothing to do
+		distinct=0 min=0 a,b _
+		chunk1 () []
 
-	s := S{}
-	res, err := s.Stripe(dests, seq, 0, 1)
+		// empty dests but nothing to do
+		distinct=0 min=0 [] a,b
+		chunk1 () []
+
+		// empty dests
+		distinct=0 min=1 [] a,b
+		chunk1 () .
+		err=ErrShort
+
+		// empty seq
+		distinct=0 min=1 a,b []
+		chunk1 () .
+		err=ErrShort
+
+		distinct=0 min=1 a,b _
+		chunk1 () a
+		chunk2 () b
+		chunk3 () a
+
+		// ignore locs outside of dests
+		distinct=0 min=1 a,b a,b,XXX
+		chunk1 () a
+		chunk2 () b
+		chunk3 () a
+
+		// reuse old locs first
+		distinct=0 min=1 a,b,c _
+		chunk1 (b) []
+		chunk2 () a
+		chunk3 () c
+
+		// ignore old locs outside of dests
+		distinct=0 min=1 a,b,c _
+		chunk1 (XXX) a
+		chunk2 () b
+		chunk3 () c
+
+		distinct=0 min=2 a,b,c _
+		chunk1 () a,b
+		chunk2 () c,a
+		chunk3 () b,c
+
+		// spread to less used first
+		distinct=0 min=2 a,b,c _
+		chunk1 (a,b) []
+		chunk2 (a) c
+		chunk3 () c,b
+
+		distinct=1 min=1 a,b,c _
+		chunk1 () a
+		chunk2 () b
+		chunk3 () c
+		chunk4 () a
+		chunk5 () b
+		chunk6 () a
+
+		distinct=1 min=1 a,b,c _
+		chunk1 (b) []
+		chunk2 (b) []
+		chunk3 () a
+
+		distinct=2 min=1 a,b,c _
+		chunk1 (b) []
+		chunk2 (b) c
+		chunk3 () a
+
+		distinct=1 min=2 a,b,c _
+		chunk1 () a,b
+		chunk2 () c .
+		err=ErrShort
+
+		distinct=1 min=2 a,b,c,d _
+		chunk1 () a,b
+		chunk2 () c,d
+		chunk3 () a,b
+		chunk4 () a,b
+
+		distinct=2 min=1 a,b,c _
+		chunk1 () a
+		chunk2 () b
+		chunk3 () c
+		chunk4 () a
+		chunk5 () a
+
+		distinct=2 min=2 a,b,c,d _
+		chunk1 () a,b
+		chunk2 () c,d
+		chunk3 () .
+		err=ErrShort
+	`)
+}
+
+func test(t *testing.T, spec string) {
+	const (
+		empty  = "[]"
+		idem   = "_"
+		strSep = ","
+	)
+	var (
+		commentRe = regexp.MustCompile(`^//`)
+		configRe  = regexp.MustCompile(`^distinct=(\d+) min=(\d+) (.+) (.+)?$`)
+		itemRe    = regexp.MustCompile(`^(.+) \((.*)\) (.+)?$`)
+		errRe     = regexp.MustCompile(`^err=(.+)$`)
+		errors    = map[string]error{
+			"ErrShort": ErrShort,
+		}
+		split = func(s string) []string {
+			switch s {
+			case "", empty:
+				return nil
+			}
+			return strings.Split(s, strSep)
+		}
+		locStrings = func(s string) (locs Locs) {
+			parts := split(s)
+			locs = make(Locs, len(parts))
+			for _, s := range parts {
+				locs.Add(s)
+			}
+			return
+		}
+		seqStrings = func(s string) Seq {
+			parts := split(s)
+			items := make([]interface{}, len(parts))
+			for i, s := range parts {
+				items[i] = s
+			}
+			return &RR{Items: items}
+		}
+	)
+	_, _, lineNr, ok := runtime.Caller(1)
+	assert.True(t, ok)
+	lineNr -= strings.Count(spec, "\n")
+	scan := bufio.NewScanner(strings.NewReader(spec))
+	for {
+		var (
+			subLine     = 0
+			blank       = 0
+			distinct    = -1
+			min         = -1
+			seq         Seq
+			dests       Locs
+			s           = make(S)
+			expected    = make(S)
+			expectedErr error
+		)
+		run := func() {
+			fmt.Printf("running test at line %d\n", lineNr)
+			fmt.Printf("  distinct=%d\n", distinct)
+			fmt.Printf("  min=%d\n", min)
+			fmt.Printf("  seq=%v\n", seq)
+			fmt.Printf("  dests=%v\n", dests)
+			fmt.Printf("  s=%v\n", s)
+			fmt.Printf("  expected=%v\n", expected)
+			fmt.Printf("  expectedErr=%v\n", expectedErr)
+			res, err := s.Stripe(dests, seq, distinct, min)
+			if expectedErr != nil {
+				assert.Equal(t, expectedErr, err, fmt.Sprintf("returned %v", res))
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, expected, res)
+			}
+			fmt.Printf("  OK\n")
+		}
+		for scan.Scan() {
+			subLine++
+			line := strings.TrimSpace(scan.Text())
+			if len(line) == 0 {
+				blank++
+				if blank > 0 && distinct > -1 {
+					run()
+					break
+				}
+				continue
+			}
+			blank = 0
+			if commentRe.MatchString(line) {
+				continue
+			}
+			if m := configRe.FindStringSubmatch(line); m != nil {
+				distinctS, minS, destsS, seqS := m[1], m[2], m[3], m[4]
+				var err error
+				distinct, err = strconv.Atoi(distinctS)
+				assert.NoError(t, err)
+				min, err = strconv.Atoi(minS)
+				assert.NoError(t, err)
+				dests = locStrings(destsS)
+				if seqS == idem {
+					seqS = destsS
+				}
+				seq = seqStrings(seqS)
+				continue
+			}
+			if m := itemRe.FindStringSubmatch(line); m != nil {
+				item := m[1]
+				_, ok := s[item]
+				assert.False(t, ok)
+				s[item] = locStrings(m[2])
+				expected[item] = locStrings(m[3])
+				continue
+			}
+			if m := errRe.FindStringSubmatch(line); m != nil {
+				err, ok := errors[m[1]]
+				assert.True(t, ok)
+				expectedErr = err
+				continue
+			}
+			t.Fatalf("invalid line: %q", line)
+		}
+		if subLine <= 0 {
+			break
+		}
+		lineNr += subLine
+	}
+	err := scan.Err()
 	assert.NoError(t, err)
-	assert.Equal(t, S{}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": nil,
-	}
-	res, err = s.Stripe(dests, seq, 0, 1)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs("a"),
-	}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("a"),
-	}
-	res, err = s.Stripe(dests, seq, 0, 1)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs(),
-	}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("x"),
-	}
-	res, err = s.Stripe(dests, seq, 0, 1)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs("a"),
-	}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": nil,
-		"chunk2": nil,
-	}
-	res, err = s.Stripe(dests, seq, 0, 2)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs("a", "b"),
-		"chunk2": NewLocs("c", "a"),
-	}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("a"),
-		"chunk2": nil,
-	}
-	res, err = s.Stripe(dests, seq, 0, 3)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs("b", "c"),
-		"chunk2": NewLocs("a", "b", "c"),
-	}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("a"),
-	}
-	res, err = s.Stripe(dests, seq, 0, 4)
-	assert.Equal(t, ErrShort, err)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("a"),
-	}
-	dests2 := NewLocs("a", "c", "d")
-	res, err = s.Stripe(dests2, seq, 0, 3)
-	assert.Equal(t, ErrShort, err)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("a"),
-		"chunk2": NewLocs("b"),
-	}
-	res, err = s.Stripe(dests, seq, 1, 2)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs("c"),
-		"chunk2": NewLocs("c"),
-	}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("b", "c"),
-		"chunk2": NewLocs("b", "c"),
-	}
-	res, err = s.Stripe(dests, seq, 1, 2)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs(),
-		"chunk2": NewLocs("a"),
-	}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("c"),
-		"chunk2": NewLocs("b"),
-	}
-	res, err = s.Stripe(dests, seq, 1, 1)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs(),
-		"chunk2": NewLocs(),
-	}, res)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("a"),
-		"chunk2": NewLocs("b"),
-	}
-	res, err = s.Stripe(dests, seq, 2, 2)
-	assert.Equal(t, ErrShort, err)
-
-	seq.Reset()
-
-	s = S{
-		"chunk1": NewLocs("a"),
-		"chunk2": NewLocs("b"),
-	}
-	dests2 = NewLocs("a", "b", "c", "d")
-	seq2 := &RR{Items: []interface{}{"a", "b", "c", "d"}}
-	res, err = s.Stripe(dests2, seq2, 2, 2)
-	assert.NoError(t, err)
-	assert.Equal(t, S{
-		"chunk1": NewLocs("c"),
-		"chunk2": NewLocs("d"),
-	}, res)
 }
