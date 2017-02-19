a06f1ef26172ea96b68a5e8f87f2ea22216d9973
Roman Le Négrate <roman.lenegrate@gmail.com>
2017-02-19 02:48:24 +0100

    cleanup

--- test/clean.rb
+++
@@ -1,8 +0,0 @@
-total = 0
-Dir.glob('out/*') do |f|
-  File.basename(f).length == "fbedda61ff8612d96d95542065666478e06307c785d82ae8fb8bd0c3f602cef7".length \
-    or next
-  File.unlink f
-  total += 1
-end
-p total: total
